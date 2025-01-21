14173d390ad7d23e1f9c5198b9f9c7c00211faac
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-01-21 23:03:29 +0100

    feat: ex complete

--- app.js
+++ app.js
@@ -1,13 +1,71 @@
 /*
 Esercizio : Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità sulla base di quello che impareremo.
 
-.. Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
+.. OK Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
 
-.. Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+.. OK Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
 
-.. Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post.
+.. OK Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post.
 
-.. Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+.. OK Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
 
 .. Testare su postman
 */
+
+// importo il modulo express installato tramite: npm install express, lines 15-30 copied from https://expressjs.com/en/starter/hello-world.html 
+const express = require('express')
+// utilizzo la funzione express per inizializzare il web server
+const app = express()
+// definisco all'interno di una costante la porta da utilizzare
+const port = 3000
+// app = webserver, .get = il metodo http su cui voglio restituire la risposta
+// il primo parametre "/" = la rotta (url) sui cui rimango in ascoloto di eventuali richieste 
+// il secondo parametre e una funzione (req, res) => {} - la funzione che gestirà la richiesta da parte del cliente e restiuirà la risposta 
+app.get('/', (req, res) => {
+  res.send('server del mio blog')
+})
+
+// chiede a express di abilitare la possibilita di leggere file statici (qui facciamo perché dobbiamo mettere le immagini) (slides 30-31 of powerpoint)
+app.use(express.static('public'));
+
+// una rotta /bacheca che restituisca un oggetto json con la lista dei post (una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe))
+app.get('/bacheca', (req, res) => {
+    res.json([ //invia i dati sotto forma di json (slide 23)
+        {
+            title: "ciambellone",
+            content: "1 novembre 2024",
+            image: 'http://localhost:3000/ciambellone.jpeg',
+            tags: ["breakfast", "dessert"],
+        },
+        {
+            title: "cracker barbabietola",
+            content: "6 novembre 2024",
+            image: 'http://localhost:3000/cracker_barbabietola.jpeg',
+            tags: ["snack"],
+        },
+        {
+            title: "pane fritto dolce",
+            content: "10 novembre 2024",
+            image: 'http://localhost:3000/pane_fritto_dolce.jpeg',
+            tags: ["dessert"],
+        },
+        {
+            title: "pasta barbabietola",
+            content: "18 novembre 2024",
+            image: 'http://localhost:3000/pasta_barbabietola.jpeg',
+            tags: ["lunch", "dinner"],
+        },
+        {
+            title: "torta paesana",
+            content: "19 novembre 2024",
+            image: 'http://localhost:3000/torta_paesana.jpeg',
+            tags: ["dessert"],
+        },
+    ])
+})
+
+
+// mette in ascolto sulla porta definita in precedenza il web server (la nostra app Back End)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
